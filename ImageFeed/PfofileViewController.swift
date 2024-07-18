0520ffaead978c004bfa49d682a5d0ce35ce7b93
Nikolay Shpilevskiy <infnick@list.ru>
2024-07-18 21:10:59 +0500

    Added profile viewcontroller

---
+++ ImageFeed/PfofileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PfofileViewController.swift
+//  ImageFeed
+//
+//  Created by Nikolay on 18.07.2024.
+//
+
+import UIKit
+
+class PfofileViewController: UIViewController {
+    
+    @IBOutlet private var photoImageView: UIImageView!
+    @IBOutlet private var emailLabel: UILabel!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var informationLabel: UILabel!
+    @IBOutlet private var exitButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
