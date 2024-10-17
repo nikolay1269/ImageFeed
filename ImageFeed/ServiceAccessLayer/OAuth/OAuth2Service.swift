7f9b6af091833781f5ef89d6768bdffb2a012865
Nikolay Shpilevskiy <infnick@list.ru>
2024-10-17 15:30:37 +0500

    Change Auth Service to exclude race condition

--- ImageFeed/ServiceAccessLayer/OAuth/OAuth2Service.swift
+++ ImageFeed/ServiceAccessLayer/OAuth/OAuth2Service.swift
@@ -7,44 +7,80 @@
 
 import Foundation
 
+enum AuthServiceError: Error {
+    case invalidRequest
+}
+
 final class OAuth2Service {
     
     static let shared = OAuth2Service()
+    private var task: URLSessionTask?
+    private var lastCode: String?
     private init() {}
     
-    private func makeOAuthTokenRequest(code: String) -> URLRequest {
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        
         let baseURL = URL(string: "https://unsplash.com")
-        let url = URL(string: "/oauth/token"
+        guard let url = URL(string: "/oauth/token"
                       + "?client_id=\(Constants.accessKey)"
                       + "&&client_secret=\(Constants.secretKey)"
                       + "&&redirect_uri=\(Constants.redirectURI)"
                       + "&&code=\(code)"
                       + "&&grant_type=authorization_code",
-                      relativeTo: baseURL)!
+                      relativeTo: baseURL)
+        else {
+            assertionFailure("Failed to create URL")
+            return nil
+        }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         return request
     }
     
-    func fetchOAuthToken(code: String, competion: @escaping (Result<String, Error>)->Void) {
+    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>)->Void) {
         
-        let request = makeOAuthTokenRequest(code: code)
-        let task = URLSession.shared.data(for: request) { result in
-            switch(result) {
-            case .success(let data):
-                do {
-                    let decoder = JSONDecoder()
-                    decoder.keyDecodingStrategy = .convertFromSnakeCase
-                    let token = try decoder.decode(OAuthTokenResponseBody.self, from: data).accessToken
-                    competion(.success(token))
-                } catch {
-                    competion(.failure(error))
-                }
-            case .failure(let error):
-                competion(.failure(error))
+        assert(Thread.isMainThread)
+        if(task != nil) {
+            if lastCode != code {
+                task?.cancel()
+            } else {
+                completion(.failure(AuthServiceError.invalidRequest))
+                return
+            }
+        } else {
+            if lastCode == code {
+                completion(.failure(AuthServiceError.invalidRequest))
+                return
             }
         }
-        
+        lastCode = code
+        guard
+            let request = makeOAuthTokenRequest(code: code)
+        else {
+            completion(.failure(AuthServiceError.invalidRequest))
+            return
+        }
+        let task = URLSession.shared.data(for: request) { [weak self] result in
+            DispatchQueue.main.async {
+                
+                switch(result) {
+                case .success(let data):
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        let token = try decoder.decode(OAuthTokenResponseBody.self, from: data).accessToken
+                        completion(.success(token))
+                    } catch {
+                        completion(.failure(error))
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+                self?.task = nil
+                self?.lastCode = nil
+            }
+        }
+        self.task = task
         task.resume()
     }
 }
